b9f096a8f6fb9ce8b5d8cba5b6c4ac911fca28f3
JeremyStanely3456778 <42357587+JeremyStanely3456778@users.noreply.github.com>
2019-01-15 12:47:17 -0500

    Update 01-fibonacci.js

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -2,5 +2,16 @@
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
 function fib(n) {
+  
+  let varOne = 1, VarTwo = 0, temporaryVar;
+
+  while (n >= 0){
+    temporaryVar = varOne;
+    varOne = varOne + VarTwo;
+    VarTwo = temporaryVar;
+    num--;
+  }
+
+  return b;
 
 }
