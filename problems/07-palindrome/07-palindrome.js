3ba6a100fd216cee56a24e59fe5d61062964156a
JeremyStanely3456778 <42357587+JeremyStanely3456778@users.noreply.github.com>
2019-01-16 16:28:44 -0500

    Update 07-palindrome.js

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -2,5 +2,26 @@
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 function isPalindrome(someStr) {
+ 
+ let splitString = someStr.split("");
+  
+ let reverseArray = splitString.reverse();
+  
+ let joinedArray = reverseArray.join("");
+
+
+  if(someStr === joinedArray){
+    return true
+  }
+  else {
+    return false
+  }
+  
   
 }
+//test code
+let variable = 'tut';
+ 
+let test = isPalindrome(variable);
+
+console.log(test);
