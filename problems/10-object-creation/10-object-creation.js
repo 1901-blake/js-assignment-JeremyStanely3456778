782fba440771a7ae23a42262e11836e3b18586f8
JeremyStanely3456778 <42357587+JeremyStanely3456778@users.noreply.github.com>
2019-01-16 21:56:56 -0500

    Update 10-object-creation.js

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -1,3 +1,12 @@
 /* 10 Object Creation
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
+class Person {
+  constructor(name, age) {
+    this.name = name;   
+    this.age = age;
+  }
+}
+let f = new Person('Lio Messi', 25);
+
+console.log(f);
